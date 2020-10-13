70824f3855ebc6e811c96c7762c685e959f6a017
CristhobalCanales <70823624+CristhobalCanales@users.noreply.github.com>
2020-10-13 18:21:34 -0300

    SE CREO LA ESTRUCTURA DEL HTML Y SE EMPEZO A TRABAJAR CON LOS ARCHIVOS JS

---
+++ js/modules/login.js
@@ -0,0 +1,25 @@
+alert('Funciona');
+
+
+export default{
+
+    
+}
+cargar: function () {
+
+    var resultado = document.getElementById('resultado')
+    var tabla = html`
+        <table>
+            <tr>
+                <th>Alumno</th>
+                <th>Puntaje</th>
+                <th>Resultado</th>
+            </tr>
+        
+        </table>
+    `
+}
+agregar: function () {
+
+
+}
