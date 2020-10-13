1559dfe5c406dba9d08be38f3f2bc6b351250036
CristhobalCanales <70823624+CristhobalCanales@users.noreply.github.com>
2020-10-13 19:10:15 -0300

    SE EMPEZO A TRABAJAR EN EL EJERCICIO NUMERO 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 alert('Funciona');
+
+import login from "./modules/login.js";
+import login from "./modules/basket.js";
+
+var caja = document.getElementById("caja");
+
+window.login = login.login;
+window.basket = basket.basket;
+
+document.getElementById("login").addEventListener("click", () => {
+caja.innerHTML = login.data;
+
+});
+
+ 
+
--- js/modules/login.js
+++ js/modules/login.js
@@ -1,25 +1,42 @@
-alert('Funciona');
 
 
 export default{
 
-    
-}
-cargar: function () {
+    alumnos: [
+        {Alumno: "Pablo Rojas",Puntaje:9,resultadologin'BUENO'}
+    ],
+    data: `
 
-    var resultado = document.getElementById('resultado')
-    var tabla = html`
-        <table>
-            <tr>
-                <th>Alumno</th>
-                <th>Puntaje</th>
-                <th>Resultado</th>
-            </tr>
+        <h1>Autor: Cristhobal Canale</h1>
+        <br>
+        Alumno <br>
+        <input id="txt1" type="text" /> <br>
+        Puntaje <br>
+        <select id="txt2>
+        <option values="0">0</option>
+        <option values="1">1</option>
+        <option values="2">2</option>
+        <option values="3">3</option>
+        <option values="4">4</option>
+        <option values="5">5</option>
+        <option values="6">6</option>
+        <option values="7">7</option>
+        <option values="8">8</option>
+        <option values="9">9</option>
+        <option values="10">10</option>
+        </select>
+        <br>
         
-        </table>
-    `
-}
-agregar: function () {
+        <button>Registrar</button>
+        <br><br>
+        <div> id="resultadologin" </div>
 
+    
+    `,
 
-}
+    cargar: function() {
+        var resultadologin = document.getElementById('resultadologin');
+    },
+    agregar: function() {},
+
+};
